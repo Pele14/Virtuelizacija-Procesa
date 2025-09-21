f032c30039403b4092284dd6a5d2b417ac035752
aleksa <aleksa.pele03@gmail.com>
2025-09-21 19:51:09 +0200

    1. tacka

---
+++ Common/IWeatherService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+  
+    
+        [ServiceContract]
+        public interface IWeatherService
+        {
+            [OperationContract]
+            string StartSession(string meta);
+
+            [OperationContract]
+            string PushSample(WeatherSample sample);
+
+            [OperationContract]
+            string EndSession();
+        }
+    }
+
---
+++ Common/WeatherSample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+
+        [DataContract]
+        public class WeatherSample
+        {
+            [DataMember] public double T { get; set; }
+            [DataMember] public double Pressure { get; set; }
+            [DataMember] public double Tpot { get; set; }
+            [DataMember] public double Tdew { get; set; }
+            [DataMember] public double VPmax { get; set; }
+            [DataMember] public double VPdef { get; set; }
+            [DataMember] public double VPact { get; set; }
+            [DataMember] public DateTime Date { get; set; }
+        }
+    }
+
+
